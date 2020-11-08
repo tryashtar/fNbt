17de64d2fbc621db30368aff245a8be34040b3cc
tryashtar <tryashtar@gmail.com>
2020-11-08 01:15:30 -0700

    Fix bytes not implementing INbtByte

--- fNbt/Tags/NbtByte.cs
+++ fNbt/Tags/NbtByte.cs
@@ -4,7 +4,7 @@ using JetBrains.Annotations;
 
 namespace fNbt {
     /// <summary> A tag containing a single byte. </summary>
-    public sealed class NbtByte : NbtTag {
+    public sealed class NbtByte : NbtTag, INbtByte {
         /// <summary> Type of this tag (Byte). </summary>
         public override NbtTagType TagType {
             get { return NbtTagType.Byte; }
